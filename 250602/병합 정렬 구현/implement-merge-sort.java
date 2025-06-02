18f04a810f42db6bf4b08a0b95e36b25c2c95bb3
dev-MIN <126967336+thals304@users.noreply.github.com>
2025-06-02 20:01:24 +0900

    Title: 병합 정렬 구현, Time: 978ms, Memory: 34MB, Status: Passed - Codetree

--- 250602/병합 정렬 구현/implement-merge-sort.java
+++ 250602/병합 정렬 구현/implement-merge-sort.java
@@ -1,31 +1,54 @@
 import java.util.Scanner;
 
-class MergeSort{
-    public int[] solution(int n, int[] arr){
-        
+class MergeSort {
+    public int[] solution(int n, int[] arr) {
         int low = 0;
         int high = n - 1;
 
         arr = merge_sort(arr, low, high);
 
         return arr;
-    } 
+    }
 
-    public int[] merge_sort(int[] arr, int low, int high){
-        if (low < high){
-            int mid = (low + mid) / 2;
+    public int[] merge_sort(int[] arr, int low, int high) {
+        if (low < high) {
+            int mid = (low + high) / 2;
             merge_sort(arr, low, mid);
             merge_sort(arr, mid + 1, high);
             merge(arr, low, mid, high);
+        }
+        return arr;
+    }
 
-            return arr;
+    public void merge(int[] arr, int low, int mid, int high) {
+        int[] temp = new int[high - low + 1];
+        int left = low;
+        int right = mid + 1;
+        int idx = 0;
+
+        while (left <= mid && right <= high) {
+            if (arr[left] <= arr[right]) {
+                temp[idx++] = arr[left++];
+            } else {
+                temp[idx++] = arr[right++];
+            }
+        }
+
+        while (left <= mid) {
+            temp[idx++] = arr[left++];
+        }
+
+        while (right <= high) {
+            temp[idx++] = arr[right++];
+        }
+
+        // 복사
+        for (int i = 0; i < temp.length; i++) {
+            arr[low + i] = temp[i];
         }
     }
-
-    public merge(int[] arr, int low, int high){
-
-    }
 }
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -34,11 +57,11 @@ public class Main {
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        // Please write your code here.
+
         MergeSort m = new MergeSort();
-        for (int x : m.solution(n, arr)){
+        int[] result = m.solution(n, arr);
+        for (int x : result) {
             System.out.print(x + " ");
         }
-
     }
 }
